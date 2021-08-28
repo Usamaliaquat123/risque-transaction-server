91f352160331cb8de3b4b7f03644afd7076160b7
Usamaliaquat123 <inconnent12345@outlook.com>
2021-08-28 22:28:46 +0500

    node fetch

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
 const express = require('express');
 
+const StellarSdk = require("stellar-sdk")
 
-
+const pair = StellarSdk.Keypair.random();
+// initialize express server 
 const app = express();
 
-app
+
+// Port which server is running on 
+var port = process.env.PORT || 8080;
+
+
+
+// Secret token
+var secretToken = pair.secret();
+// Public Token
+var publicToken = pair.publicKey()
+
+
+// lumen horizon server
+
+
+const main = async () => {
+    try {
+        const response = await fetch(
+            `https://friendbot.stellar.org?addr=${encodeURIComponent(
+                publicToken,
+            )}`,
+          );
+          const responseJSON = await response.json();
+          console.log("SUCCESS! You have a new account :)\n", responseJSON);
+    }catch(err) { console.error(`ERROR ${err}`) }
+} 
+
+
+main()
+
+// var server = new StellarSdk.Server('https://horizon-testnet.stellar.org')
+
+
+
+console.log(secretToken)
+console.log(publicToken)
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`port is running on ${port}`)
+})
